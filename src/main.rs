ea26c70f7fff5e2c562f51f2ff69d34add971d47
XXXMrG <mrgitservice@outlook.com>
2023-11-17 18:49:14 +0800

    dev: some code

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,73 @@
 use image::{GenericImage, GenericImageView, ImageBuffer, Pixel, Rgb, RgbImage};
 use rand::distributions::Uniform;
 use rand::prelude::*;
-use std::env::{self, var};
+use std::env;
 use std::path::Path;
+
+type Point3D = [i32; 3];
+type Point2D = [i32; 2];
+
+fn dot_product(a: Point3D, b: Point3D) -> f32 {
+    (a[0] * b[0] + a[1] * b[1] + a[2] * b[2]) as f32
+}
+
+fn subtract(a: Point3D, b: Point3D) -> Point3D {
+    [a[0] - b[0], a[1] - b[1], a[2] - b[2]]
+}
+
+fn intersect_ray_sphere(origin: Point3D, direction: Point3D, sphere: &Sphere) -> [f32; 2] {
+    let sphere_to_origin = subtract(origin, sphere.center);
+
+    let k1 = dot_product(direction, direction);
+    let k2 = 2.0 * dot_product(sphere_to_origin, direction);
+    let k3 = dot_product(sphere_to_origin, sphere_to_origin) - sphere.radius * sphere.radius;
+
+    let discriminant = (k2 * k2 - 4.0 * k1 * k3) as f32;
+    if discriminant < 0.0 {
+        return [f32::MAX, f32::MAX];
+    }
+
+    let t1 = 0.5 * (-k2 - discriminant.sqrt()) / k1;
+    let t2 = 0.5 * (-k2 + discriminant.sqrt()) / k1;
+    [t1, t2]
+}
+
+struct Sense {
+    canvas: Canvas,
+    viewport_size: i32,
+    projection_plane_z: i32,
+    camera_pos: Point3D,
+    back_color: Rgb<u8>,
+    spheres: Vec<Sphere>,
+}
+
+impl Sense {
+    fn new(canvas: Canvas, spheres: Vec<Sphere>) -> Sense {
+        Sense {
+            canvas,
+            spheres,
+            viewport_size: 1,
+            projection_plane_z: 1,
+            camera_pos: [0; 3],
+            back_color: Rgb([0; 3]),
+        }
+    }
+
+    fn canvas_to_viewport(&self, point2d: Point2D) -> Point3D {
+        let [x, y] = point2d;
+        [
+            x * self.viewport_size / self.canvas.width as i32,
+            y * self.viewport_size / self.canvas.height as i32,
+            self.projection_plane_z,
+        ]
+    }
+}
+
+struct Sphere {
+    center: Point3D,
+    radius: f32,
+    color: Rgb<u8>,
+}
 
 struct Canvas {
     width: u32,
